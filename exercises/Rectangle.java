03938e6cb73b2336d30b63d722b2557a4f6d935a
idir khaldi <khaldiidir8@gmail.com>
2026-05-15 18:18:35 +0200

    Add files via upload

---
+++ exercises/Rectangle.java
@@ -0,0 +1,16 @@
+
+public class Rectangle {
+    public double hauteur=2;
+    public double largeur=3 ;
+
+    public double surface() {
+        return hauteur*largeur ;
+        }
+}
+public void main(String[] args) {
+    Rectangle r ;
+    System.out.println(r.surface());
+}
+
+
+
